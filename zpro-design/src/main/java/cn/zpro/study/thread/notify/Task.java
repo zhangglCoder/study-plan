5adfb22f22de018196801dcdf9260bbdc6a7cc71
guolongzhang <guolongzhang@ebnew.com>
2017-08-17 16:49:06 +0800

    基础练习

--- zpro-design/src/main/java/cn/zpro/study/thread/notify/Task.java
+++ zpro-design/src/main/java/cn/zpro/study/thread/notify/Task.java
@@ -3,46 +3,38 @@ package cn.zpro.study.thread.notify;
 public class Task {
     public static boolean flag = true;
 
-    public void doTask1(){
-            synchronized (Task.this){
-                if(!flag) {
-                    try {
-                        System.out.println("子线程进入等待状态...");
-                        this.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(Thread.currentThread().getName() + " 子线程 ->" + i);
-                }
-                flag = false;
-                this.notify();
-
+    public synchronized void doTask1() {
+        while (!flag) {
+            try {
+                System.out.println("子线程进入等待状态...");
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " 子线程 ->" + i);
+        }
+        flag = false;
+        this.notify();
+
 
     }
 
-    public void doTask2(){
-        synchronized (Task.this){
-            if(flag){
-                try {
-                    System.out.println("主线程进入等待状态...");
-                    this.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    public synchronized void doTask2() {
+        while (flag) {
+            try {
+                System.out.println("主线程进入等待状态...");
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            for (int i = 0; i < 100; i++) {
-                System.out.println(Thread.currentThread().getName() + " 主线程 ->" + i);
-            }
-            flag = true;
-            this.notify();
-
-
         }
-
-
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " 主线程 ->" + i);
+        }
+        flag = true;
+        this.notify();
 
 
     }
