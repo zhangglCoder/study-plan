11d5de89e2caaaa6f9fb491db697ef02c60180bf
guolongzhang <guolongzhang@ebnew.com>
2017-11-04 12:53:26 +0800

    init

--- zpro-interview/src/main/java/cn/zpro/interview/service/ContractService.java
+++ zpro-interview/src/main/java/cn/zpro/interview/service/ContractService.java
@@ -36,16 +36,43 @@ public class ContractService {
             Calendar instance = Calendar.getInstance();
             instance.setTime(idate);
             instance.add(Calendar.MONTH , paymentMethod);
+            instance.add(Calendar.DATE,-1);
+            if(termTimeEnd.compareTo(instance.getTime())>0){
+                receipt.setHouseMoneyEndDay(instance.getTime());
+            }else {
+                receipt.setHouseMoneyEndDay(termTimeEnd);
+            }
+
+            instance.add(Calendar.DATE,1);
             receipt.setHouseMoneyStartDay(idate);
-            receipt.setHouseMoneyEndDay(instance.getTime());
-            receipt.setRent(paymentMethod * monthlyRent);
+            setRentInfo(receipt.getHouseMoneyStartDay(),receipt.getHouseMoneyEndDay(),receipt,monthlyRent);
             receipt.setReceiptOfDate(idate);
-            receipt.setYear(instance.get(Calendar.YEAR));
+            Calendar instance_year = Calendar.getInstance();
+            instance_year.setTime(receipt.getReceiptOfDate());
+            receipt.setYear(instance_year.get(Calendar.YEAR));
             yearReceiptList.add(receipt);
-            idate = receipt.getHouseMoneyEndDay();
+            idate = instance.getTime();
         }
         return yearReceiptList;
 
+    }
+    private void setRentInfo(Date start,Date end,YearReceipt receipt,double monthlyRent){
+        double money;
+        //2018-02-28
+        //2018-03-01
+        int mouth = getMouth(start, end);
+        int date = differentDays(start,end) + 1;
+
+        //如果足月
+        if(date > 28 ){
+            money = mouth * monthlyRent;
+        }else {
+            //日租金
+            double dayRent = monthlyRent / 30;
+            money = dayRent * date;
+        }
+        //如果不足月
+        receipt.setRent(money);
     }
 
     /**
@@ -115,9 +142,69 @@ public class ContractService {
         instanceTermTimeEnd.setTime(termTimeEnd);
         int y = instanceTermTimeEnd.get(Calendar.YEAR) - instanceTermTimeStart.get(Calendar.YEAR);
         int m = instanceTermTimeEnd.get(Calendar.MONTH) - instanceTermTimeStart.get(Calendar.MONTH);
+
         if (y > 0) {
             m += y * 12;
         }
         return m;
+    }
+
+    private int getDate(Date termTimeStart, Date termTimeEnd){
+        Calendar instanceTermTimeStart = Calendar.getInstance();
+        Calendar instanceTermTimeEnd = Calendar.getInstance();
+        instanceTermTimeStart.setTime(termTimeStart);
+        instanceTermTimeEnd.setTime(termTimeEnd);
+        int y = instanceTermTimeEnd.get(Calendar.YEAR) - instanceTermTimeStart.get(Calendar.YEAR);
+        int m = instanceTermTimeEnd.get(Calendar.MONTH) - instanceTermTimeStart.get(Calendar.MONTH);
+        int r = instanceTermTimeEnd.get(Calendar.DATE) - instanceTermTimeStart.get(Calendar.DATE);
+        if (y > 0) {
+            m += y * 12;
+        }
+        if(m>0 ){
+            r += m * 30;
+        }
+        return r;
+    }
+
+    /**
+     * date2比date1多的天数
+     * @param date1
+     * @param date2
+     * @return
+     */
+    public static int differentDays(Date date1,Date date2)
+    {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        int day1= cal1.get(Calendar.DAY_OF_YEAR);
+        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+        int year1 = cal1.get(Calendar.YEAR);
+        int year2 = cal2.get(Calendar.YEAR);
+        if(year1 != year2)   //同一年
+        {
+            int timeDistance = 0 ;
+            for(int i = year1 ; i < year2 ; i ++)
+            {
+                if(i%4==0 && i%100!=0 || i%400==0)    //闰年
+                {
+                    timeDistance += 366;
+                }
+                else    //不是闰年
+                {
+                    timeDistance += 365;
+                }
+            }
+
+            return timeDistance + (day2-day1) ;
+        }
+        else    //不同年
+        {
+            //System.out.println("判断day2 - day1 : " + (day2-day1));
+            return day2-day1;
+        }
     }
 }
