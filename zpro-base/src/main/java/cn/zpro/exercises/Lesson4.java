539e7dcc0d0aba4fde496ec830ceeacb1ef48430
guolongzhang <guolongzhang@ebnew.com>
2017-12-20 17:45:54 +0800

    完数

---
+++ zpro-base/src/main/java/cn/zpro/exercises/Lesson4.java
@@ -0,0 +1,23 @@
+package cn.zpro.exercises;
+
+/**
+ * @author zhanggl
+ */
+public class Lesson4 {
+
+    /**
+     题目：一个数如果恰好等于它的因子之和，这个数就称为"完数"。
+     例如6=1＋2＋3.编程找出1000以内的所有完数。
+
+     * @param args
+     */
+    public static void main(String[] args) {
+        sun();
+    }
+    public static void sun(){
+
+
+    }
+
+
+}
