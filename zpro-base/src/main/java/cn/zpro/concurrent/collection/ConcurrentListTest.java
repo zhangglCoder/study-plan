dc322939d5ccc4fae87c44a80b3874a83c637adb
guolongzhang <guolongzhang@ebnew.com>
2018-02-05 10:52:19 +0800

    判断对象

--- zpro-base/src/main/java/cn/zpro/concurrent/collection/ConcurrentListTest.java
+++ zpro-base/src/main/java/cn/zpro/concurrent/collection/ConcurrentListTest.java
@@ -1,28 +1,26 @@
 package cn.zpro.concurrent.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 并发修改集合-综合
  * @author zhanggl
  */
-public class ListTest {
+public class ConcurrentListTest {
 
     public static void main(String[] args) {
         CountDownLatch downLatch = new CountDownLatch(10);
-        List<String> list = new ArrayList<>();
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
         list.add("2");
         list.add("2");
         list.add("11");
         list.add("11");
         list.add("3");
         list.add("3");
-        for (int i=0;i<10;i++){
+        for (int i = 0; i < 10; i++) {
             doTask(downLatch, list);
         }
     }
@@ -43,25 +41,13 @@ public class ListTest {
     }
 
     private static void deleteItem(List<String> list) {
-        Lock lock = new ReentrantLock();
         Iterator<String> iterator = list.iterator();
-        lock.lock();
-        try {
-            while (iterator.hasNext()) {
-                String val = iterator.next();
-                if (val.equals("2")) {
-                    iterator.remove();
-                }
+        while (iterator.hasNext()) {
+            String val = iterator.next();
+            if (val.equals("2")) {
+                iterator.remove();
             }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            lock.unlock();
         }
 
-
-
     }
-
-
 }
